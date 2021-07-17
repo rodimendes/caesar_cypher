ce7153a9f7ece57c7449ca2ff70b1e9fecf72911
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2021-07-17 10:07:15 +0000

    Simplifiado

--- main.py
+++ main.py
@@ -4,26 +4,20 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(pure_text, shift_amount):
-    encoded = ""
-    for letter in pure_text:
+def caesar(text, shift_amount, cypher_direction):
+    new_text = ""
+    for letter in text:
         index = alphabet.index(letter)
-        new_position = index + shift_amount
-        if new_position > 25:
-            new_position -= 26
-        encoded += alphabet[new_position]
-    print(f"The encoded text is '{encoded}'")
-    return encoded
-
-def decrypt(enc_text, shift_amount):
-    decoded = ""
-    for letter in enc_text:
-        index = alphabet.index(letter)
-        new_position = index - shift_amount
-        if new_position < 0:
-            new_position = 26 + new_position
-        decoded += alphabet[new_position]
-    print(f"The decoded text is '{decoded}''")
-
-cipher = encrypt(text, shift)
-decipher = decrypt(cipher, shift)
+        if cypher_direction == "encode":
+            new_position = index + shift_amount
+            if new_position > 25:
+                new_position -= 26
+            new_text += alphabet[new_position]
+        elif cypher_direction == "decode":
+            new_position = index - shift_amount
+            if new_position < 0:
+                new_position = 26 + new_position
+            new_text += alphabet[new_position]
+            
+    print(f"The {cypher_direction}d text is '{new_text}'")
+caesar(text, shift, direction)
